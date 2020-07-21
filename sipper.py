b6b6f2f05be0dd3294ba0068902fa83782216f7e
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2020-07-21 16:39:57 -0500

    figuring out assigning contents

--- sipper.py
+++ sipper.py
@@ -1,10 +1,11 @@
 """Class for loading sipper data."""
 
+import numpy as np
 import os
 import pandas as pd
 
-class SipperLoadError(Exception):
-    """Error for sipper loading errors"""
+class SipperError(Exception):
+    """Class for sipper errors"""
 
 class Sipper():
     def __init__(self, path):
@@ -17,7 +18,7 @@ class Sipper():
                                            'LeftCount', 'LeftDuration',
                                            'RightCount', 'RightDuration',
                                            'BatteryVoltage']:
-                raise SipperLoadError('Column names do not match sipper data')
+                raise SipperError('Column names do not match sipper data')
         except FileNotFoundError as error:
             raise error
         except pd.errors.EmptyDataError as error:
@@ -32,6 +33,8 @@ class Sipper():
         self.data['MM:DD:YYYY hh:mm:ss'] = pd.to_datetime(self.data['MM:DD:YYYY hh:mm:ss'])
         self.data['Elapsed Time'] = pd.to_timedelta(self.data['Elapsed Time'])
         self.data = self.data.set_index('MM:DD:YYYY hh:mm:ss')
+        self.data['LeftContents'] = np.nan
+        self.data['RightContents'] = np.nan
         self.start_date = self.data.index[0]
         self.end_date = self.data.index[-1]
         self.duration = self.end_date - self.start_date
@@ -42,7 +45,30 @@ class Sipper():
         self.device_no = self.data['Device'][0]
         self.left_name = 'Left'
         self.right_name = 'Right'
+        self.contents = []
 
     def __repr__(self):
         """Shows the directory used to make the file."""
         return 'Sipper("' + self.path + '")'
+
+    def assign_contents(self, d):
+        self.contents = []
+        for (start, end), (left, right) in d.items():
+            if start not in self.data.index:
+                before = self.data.index[self.data.index < start].max()
+                if not pd.isna(before):
+                    self.data.loc[start, :] = np.nan
+                    self.data.loc[start, :] = self.data.loc[before,:]
+            self.data.loc[(self.data.index >= start) &
+                          (self.data.index < end), 'LeftContents'] = left
+            self.data.loc[(self.data.index >= start) &
+                          (self.data.index < end), 'RightContents'] = right
+        self.data.sort_index(inplace=True)
+        all_left = self.data['LeftContents'].unique()
+        all_right = self.data['RightContents'].unique()
+        self.contents = list(np.unique(np.concatenate([all_left,all_right])))
+
+    def get_drinkcount(self, content):
+        subset = self.data[(content in self.data['LeftContents']) |
+                           (content in self.data['RightContents'])]
+
--- sipper_testing.py
+++ sipper_testing.py
@@ -1,41 +1,28 @@
 import sipper
 import sipperplots
+import pandas as pd
 
 path = r"C:\Users\earne\Box\20200313 Behavior Study sipper data\4\SIP004_030320_00.CSV"
 path2 = r"C:\Users\earne\Box\20200313 Behavior Study sipper data\4\SIP005_020320_01.CSV"
 
+
+t1 = pd.Timestamp(year=2020, month=3, day=3, hour=13)
+t2 = pd.Timestamp(year=2020, month=3, day=3, hour=21)
+t3 = pd.Timestamp(year=2020, month=3, day=4, hour=0)
+t4 = pd.Timestamp(year=2020, month=3, day=4, hour=4)
+
 s = sipper.Sipper(path)
+s.assign_contents({(t1,t2):("Water","Oxy"),
+                   (t2,t3):("Oxy","Water"),
+                   (t3,t4):('Water',"Oxy")})
 d = s.data
 
-s2 = sipper.Sipper(path2)
-d2 = s2.data
+d['LeftContents'].ne(d['LeftContents'].shift().bfill()).astype(int).cumsum()
 
-sipperplots.drinkcount(s2, left=True, right=True, shade_dark=True,
-                       lights_on=7, lights_off=19)
+# s2 = sipper.Sipper(path2)
 
-#%%
-from tkinter import ttk, Tk, Toplevel
+# d2 = s2.data
 
-root = Tk()
-welcome_window = Toplevel(root)
-welcome_window.title('Welcome')
+# sipperplots.drinkcount(s2, left=True, right=True, shade_dark=True,
+#                        lights_on=7, lights_off=19)
 
-lab_window = Toplevel(root)
-lab_window.title('Lab')
-
-root.withdraw() # hide root window
-lab_window.withdraw() # hide lab window
-
-def goto_lab():
-    welcome_window.destroy()
-    lab_window.deiconify() # show lab window
-
-button1 = ttk.Button(welcome_window, text='Close',\
-                     command=goto_lab)
-button1.pack(padx=100, pady=50)
-
-button2 = ttk.Button(lab_window, text='Close',\
-                     command=quit)
-button2.pack(padx=100, pady=50)
-
-root.mainloop()
